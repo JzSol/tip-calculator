6df6156dc8b3d5b2cc085ca698535d24a8c3dc41
Jānis Žurilo <zurilojohn@gmail.com>
2023-04-27 13:56:21 +0300

    added functionality

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -1,22 +1,63 @@
+import { useEffect, useState, useMemo } from 'react';
 import { Interactiveblock } from './Interactiveblock';
-
 import './body.scss';
+import classNames from 'classnames';
 
 export const Body = () => {
+  const [bill, setBill] = useState<string>('');
+  const [count, setCount] = useState<string>('');
+  const [customPerc, setCustomPerc] = useState<string>('');
+  const [total, setTotal] = useState(0);
+  const [tipAmm, setTipAmm] = useState(0);
+  const [percBtn, setPercBtn] = useState(0);
+
+  useEffect(() => {
+    const inputCheck = bill.length > 0 && count.length > 0;
+    if (inputCheck) {
+      setTipAmm((Number(bill) * Number(customPerc) || percBtn) / 100);
+      setTotal(Number(bill) / Number(count) + Number(tipAmm));
+    }
+  }, [bill, count, tipAmm, customPerc, percBtn]);
+
+  const handleReset = () => {
+    setBill('');
+    setCount('');
+    setCustomPerc('');
+    setTotal(0);
+    setTipAmm(0);
+    setPercBtn(0);
+  };
+
+  const resetStatus = useMemo(() => {
+    return bill.length > 0 || count.length > 0 || customPerc.length > 0;
+  }, [bill, count, customPerc]);
+
   return (
     <body className='body'>
       <main className='container'>
-        <Interactiveblock />
+        <Interactiveblock
+          bill={bill}
+          setBill={setBill}
+          count={count}
+          setCount={setCount}
+          customPerc={customPerc}
+          setCustomPerc={setCustomPerc}
+          setPercBtn={setPercBtn}
+        />
         <button className='group11'>
-          <div className='resetbtn'>reset</div>
-          <div className='total'>$32.79</div>
+          <div
+            className={classNames('resetbtn', { notActive: !resetStatus })}
+            onClick={handleReset}>
+            reset
+          </div>
+          <div className='total'>${total}</div>
           <div className='textblock'>
             <div className='tot'>
               Total<div className='person'>/person</div>
             </div>
           </div>
 
-          <div className='tipAm'>$4.27</div>
+          <div className='tipAm'>${tipAmm}</div>
           <div className='textblock1'>
             <div className='tot1'>
               Tip Amount<div className='person1'>/person</div>
--- src/components/Interactiveblock.tsx
+++ src/components/Interactiveblock.tsx
@@ -1,14 +1,60 @@
 import './container1.scss';
 
-export const Interactiveblock = () => {
+type props = {
+  bill: string;
+  setBill: React.Dispatch<React.SetStateAction<string>>;
+  count: string;
+  setCount: React.Dispatch<React.SetStateAction<string>>;
+  customPerc: string;
+  setCustomPerc: React.Dispatch<React.SetStateAction<string>>;
+  setPercBtn: React.Dispatch<React.SetStateAction<number>>;
+};
+
+export const Interactiveblock: React.FC<props> = ({
+  bill,
+  setBill,
+  count,
+  setCount,
+  customPerc,
+  setCustomPerc,
+  setPercBtn,
+}) => {
+  const handleBillChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setBill(event.target.value);
+  };
+
+  const handlePeopleCount = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setCount(event.target.value);
+  };
+
+  const handlePercentChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setCustomPerc(event.target.value);
+  };
+
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    // Allow only numbers to be entered
+    const isNumber = /^[0-9]$/.test(event.key);
+    const isDelete = event.key === 'Backspace' || event.key === 'Delete';
+    if (!isNumber && !isDelete) {
+      event.preventDefault();
+    }
+  };
+
   return (
     <div className='interactiveblock'>
       <div className='bill'>bill</div>
 
-      <div className='inputbox'>
+      <div className='inputbox '>
         <label>
-          <input className='input' type='text' value={142.55}></input>
-
+          <input
+            className='input activebill'
+            type='text'
+            placeholder={'0'}
+            pattern='[0-9]'
+            value={bill}
+            onChange={handleBillChange}
+            onKeyDown={handleKeyDown}
+          />
           <div className='dollar'></div>
         </label>
       </div>
@@ -17,24 +63,39 @@ export const Interactiveblock = () => {
         <div className='select'>Select Tip %</div>
 
         <div className='btnbox'>
-          <button className='btn'>
-            <div className='btnper'>5% </div>
+          <button
+            className='btn'
+            onClick={() => setPercBtn(50)}>
+            5%
           </button>
-          <button className='btn'>
-            <div className='btnper'>25% </div>
+          <button
+            className='btn'
+            onClick={() => setPercBtn(250)}>
+            25%
           </button>
-          <button className='btn'>
-            <div className='btnper'>10% </div>
+          <button
+            className='btn'
+            onClick={() => setPercBtn(100)}>
+            10%
           </button>
-          <button className='btn'>
-            <div className='btnper'>50% </div>
+          <button
+            className='btn'
+            onClick={() => setPercBtn(500)}>
+            50%
           </button>
-          <button className='btn'>
-            <div className='btnper'>15% </div>
+          <button
+            className='btn'
+            onClick={() => setPercBtn(150)}>
+            15%
           </button>
-          {/* <button className='btncustom'>
-            <div className='custom'>custom</div>
-          </button> */}
+          <input
+            className='custbtn'
+            type='text'
+            placeholder='Custom'
+            value={customPerc}
+            onChange={handlePercentChange}
+            onKeyDown={handleKeyDown}
+          />
         </div>
       </div>
 
@@ -43,7 +104,15 @@ export const Interactiveblock = () => {
 
         <div className='input2'>
           <label>
-            <input type='text' className='countinput' value={5}></input>
+            <input
+              type='text'
+              className='countinput'
+              placeholder={'0'}
+              pattern='[0-9]'
+              value={count}
+              onChange={handlePeopleCount}
+              onKeyDown={handleKeyDown}
+            />
             <div className='icon'></div>
           </label>
         </div>
